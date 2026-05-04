9aecae8034ec9d912693be29f54458ce158c5ded
Hyunews <warmsky02@gmail.com>
2026-05-04 18:45:18 +0900

    11

--- raspberry/ui/threads/face_thread.py
+++ raspberry/ui/threads/face_thread.py
@@ -186,11 +186,10 @@ class FaceThread(QThread):
 
         if self._running:
             self.capture_done.emit(face_imgs)
-        gimbal.stop()
- else:
+            gimbal.stop()
+        else:
                 # 얼굴이 없으면 2초 후 정위치 복귀 체크
-                gimbal.update_idle()
-            
+            gimbal.update_idle()
             self.msleep(10)
 
         if self._running:
